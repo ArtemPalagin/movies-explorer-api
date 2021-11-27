d6d5399e3c4906701f319eb2bc6244b263b45208
ArtemPalagin <art.palagin@gmail.com>
2021-11-27 21:11:15 +0300

    email

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ router.get('/users/me', getUser);
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    emale: Joi.string().required().email(),
+    email: Joi.string().required().email(),
   }),
 }), patchProfile);
 
