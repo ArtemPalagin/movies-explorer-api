8d43dd38a9cdd2324b0284136d53921a87a9aa5c
ArtemPalagin <art.palagin@gmail.com>
2021-11-29 16:04:07 +0300

    validator

--- models/movie.js
+++ models/movie.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { validator } = require('validator');
 
 const movieSchema = new mongoose.Schema({
   country: {
@@ -26,7 +27,7 @@ const movieSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator(v) {
-        return /^https?:\/\/(www\.)?([\w\-.~:?#[\]@!$&'()*+,;=]){1,}#?/i.test(v);
+        return validator.isURL(v);
       },
       message: 'Такой ссылки не может быть',
     },
@@ -36,7 +37,7 @@ const movieSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator(v) {
-        return /^https?:\/\/(www\.)?([\w\-.~:?#[\]@!$&'()*+,;=]){1,}#?/i.test(v);
+        return validator.isURL(v);
       },
       message: 'Такой ссылки не может быть',
     },
@@ -46,7 +47,7 @@ const movieSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator(v) {
-        return /^https?:\/\/(www\.)?([\w\-.~:?#[\]@!$&'()*+,;=]){1,}#?/i.test(v);
+        return validator.isURL(v);
       },
       message: 'Такой ссылки не может быть',
     },
--- routes/movie.js
+++ routes/movie.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
+const { validator } = require('validator');
 const {
   getMovies, postMovie, deleteMovie,
 } = require('../controllers/movie');
@@ -12,20 +13,23 @@ router.post('/movies', celebrate({
     duration: Joi.number().required(),
     year: Joi.string().required(),
     description: Joi.string().required(),
-    image: Joi.string().required().uri({
-      scheme: [
-        'http', 'https',
-      ],
+    image: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Поле image заполнено некорректно');
     }),
-    trailer: Joi.string().required().uri({
-      scheme: [
-        'http', 'https',
-      ],
+    trailer: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Поле trailer заполнено некорректно');
     }),
-    thumbnail: Joi.string().required().uri({
-      scheme: [
-        'http', 'https',
-      ],
+    thumbnail: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Поле thumbnail заполнено некорректно');
     }),
     movieId: Joi.string().required(),
     nameRU: Joi.string().required(),
@@ -34,7 +38,7 @@ router.post('/movies', celebrate({
 }), postMovie);
 router.delete('/movies/:movieId', celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().alphanum().length(24),
+    movieId: Joi.string().hex().length(24),
   }),
 }), deleteMovie);
 
