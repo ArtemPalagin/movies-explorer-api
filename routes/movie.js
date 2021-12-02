906def945a36d3d7cc3e386da7cd917c51f37812
ArtemPalagin <art.palagin@gmail.com>
2021-12-02 20:45:08 +0300

    validator

--- routes/movie.js
+++ routes/movie.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { validator } = require('validator');
+const validator = require('validator');
 const {
   getMovies, postMovie, deleteMovie,
 } = require('../controllers/movie');
